78947172fe07d30573ce31e15dc75700d28d378a
Hiuming <46265075+Hiuming@users.noreply.github.com>
2021-10-25 17:12:23 +0700

    Add files via upload

--- home/home_header.dart
+++ home/home_header.dart
@@ -12,59 +12,78 @@ class homeHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-            height: size.height / 5,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.blue,
+    return Column(children: <Widget>[
+      Container(
+          height: size.height / 5,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+          ),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 10),
+              Text(
+                "Total lesson time is 2 hours 46 minutes",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Upcoming lessons",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Wed, 06 Oct 21 06:30 - 06:55 ",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SizedBox(height: 20),
+              in_cell_btn(
+                  text: Text(
+                    "Enter lesson room",
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  color: Colors.white,
+                  size: Size(200, 40),
+                  fill: Colors.white)
+            ],
+          )),
+      SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
+              child: teacherCellHeader(
+                image: 'assets/images/userProfile.jpg',
+              ),
             ),
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 10),
-                Text(
-                  "Total lesson time is 2 hours 46 minutes",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "Upcoming lessons",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "Wed, 06 Oct 21 06:30 - 06:55 ",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                SizedBox(height: 20),
-                in_cell_btn(
-                    text: Text(
-                      "Enter lesson room",
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                    color: Colors.white,
-                    size: Size(200, 40),
-                    fill: Colors.white)
-              ],
-            )),
-        SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Padding(
-                padding: EdgeInsets.only(top: 2), child: teacherListItems())),
-      ],
-    );
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
+              child: teacherCellHeader(
+                image: 'assets/images/userProfile.jpg',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
+              child: teacherCellHeader(
+                image: 'assets/images/userProfile.jpg',
+              ),
+            ),
+          ],
+        ),
+      )
+    ]);
   }
 }
